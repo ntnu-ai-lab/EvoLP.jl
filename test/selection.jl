43eedaf4c08dc638764f8113fbb5a33e2917f93a
saxarona <5231167+saxarona@users.noreply.github.com>
2024-01-11 02:40:51 +0100

    Transport of selection test suite

--- test/selection.jl
+++ test/selection.jl
@@ -20,4 +20,18 @@ fits = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
         @test s == [2, 2]
         @test length(s) == 2
     end
-end
+
+    @testset "Roulette Wheel Selector" begin
+        R = RouletteWheelSelector()
+        s = select(R, fits; rng=myrng) # rand(1:10, 2) = [6, 4]
+        @test s == [6, 4]
+        @test length(s) == 2
+    end
+
+    @testset "Rank based Selector" begin
+        R = RankBasedSelector()
+        s = select(R, fits; rng=myrng) # rand(1:10, 2) = [2, 8]
+        @test s == [2, 8]
+        @test length(s) == 2
+    end
+end;
